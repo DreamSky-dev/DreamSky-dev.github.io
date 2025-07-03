b168194a3ad6d154ba571f170b49db01efa7bf78
Chris Redfield <flyawaytosky2025@gmail.com>
2025-07-03 04:45:15 -0400

    add export setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  output: 'export', // This enables static HTML export
+  trailingSlash: true, // Ensures paths have trailing slashes (required by GitHub Pages)
+};
